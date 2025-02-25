bb9fc9987211f1bb89819e1db684621c315401c7
bwu1205 <bwu1205@gmail.com>
2025-02-25 15:47:58 -0800

    added one hot encoding to switch categories into numerical values

--- Backend/Model/dataset.py
+++ Backend/Model/dataset.py
@@ -6,6 +6,7 @@ from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.preprocessing import OneHotEncoder
 
 
+
 df = pd.read_csv('vgchartz-2024.csv', encoding='latin-1')
 
 DATABASE_URI = 'postgresql+psycopg2://postgres:postgres@localhost:5432/gamesuccessml'
@@ -42,7 +43,17 @@ dftable['Hit'] = (dftable['total_sales'] >= hitflop_percentile).astype(int)
 X = dftable[['console', 'genre', 'critic_score']]
 y = dftable['Hit']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+
+one_hot_encoder = OneHotEncoder(sparse_output=False, handle_unknown='ignore')
+
+X_Encode = one_hot_encoder.fit_transform(X[['console', 'genre']])
+
+X_df = pd.DataFrame(X_Encode, columns = one_hot_encoder.get_feature_names_out(['console', 'genre']))
+
+Xnum = pd.concat([X[['critic_score']].reset_index(drop = True), X_df], axis=1)
+
+
+X_train, X_test, y_train, y_test = train_test_split(Xnum, y, test_size = 0.2, random_state = 42)
 
 model = DecisionTreeClassifier(max_depth = 5, random_state = 42)
 model.fit(X_train, y_train)
@@ -61,8 +72,17 @@ new_game = {
     'critic_score': 85
 }
 new_game_df = pd.DataFrame([new_game])
-new_game_df = new_game_df[['console', 'genre', 'critic_score']]
-prediction = model.predict(new_game_df)
+
+# new_game_df = new_game_df[['console', 'genre', 'critic_score']]
+new_gamenum = one_hot_encoder.transform(new_game_df[['console', 'genre']])
+
+new_gamenum_df = pd.DataFrame(new_gamenum, columns = one_hot_encoder.get_feature_names_out(['console', 'genre']))
+
+newgame_fin = pd.concat([new_game_df[['critic_score']].reset_index(drop = True), new_gamenum_df], axis = 1)
+
+# prediction = model.predict(new_game_df)
+prediction = model.predict(newgame_fin)
+
 print("Prediction: Hit" if prediction[0] == 1 else "Prediction: Flop")
 
 print(dftable)
