ec01fb7f3597990a8ec6e30b614e14038cb4813c
bwu1205 <bwu1205@gmail.com>
2025-02-27 14:34:04 -0800

    Added server app routing functionality

--- Backend/server.py
+++ Backend/server.py
@@ -6,4 +6,22 @@ app = Flask(__name__)
 
 @app.route('/submit', methods=['POST'])
 def submit():
+    data = request.get_json()
+    console = data['console']
+    genre = data['genre']
+    critic_score = data['critic_score']
+
+    df = pd.DataFrame({
+        'console': [console],
+        'genre': [genre],
+        'critic_score': [critic_score]
+    })
     
+    scaler = StandardScaler()
+    scaler_data = scaler.fit_transform(df)
+
+    # df['critic_score'] = scaler.fit_transform(df[['critic_score']])
+    return jsonify({'message': 'Data received successfully', 'scaler_data': scaler_data.tolist()})
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
